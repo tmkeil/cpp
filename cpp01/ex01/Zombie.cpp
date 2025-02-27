4714b7f8c0e7656cf03f8088dc68178398400f3f
Tobias Keil <tkeil@student.42heilbronn.de>
2025-02-27 11:26:15 +0100

    added announce member function in ex01.

--- cpp01/ex01/Zombie.cpp
+++ cpp01/ex01/Zombie.cpp
@@ -6,14 +6,24 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 12:33:05 by tkeil             #+#    #+#             */
-/*   Updated: 2025/02/18 12:54:45 by tkeil            ###   ########.fr       */
+/*   Updated: 2025/02/26 13:56:31 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void    initializeZombies( Zombie *zombies, int N, std::string name ) {
-    for (int i = 0; i < N; i++) {
-        zombies[i] = Zombie(name);
-    }
+Zombie::Zombie() {}
+
+Zombie::Zombie(std::string name): _name(name) {}
+
+Zombie::~Zombie() {}
+
+void Zombie::announce() const {
+    std::cout << this->_name << + ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+void initializeZombies(Zombie* zombies, int N, std::string name) {
+  for (int i = 0; i < N; i++) {
+    zombies[i] = Zombie(name);
+  }
 }
--- cpp01/ex01/Zombie.hpp
+++ cpp01/ex01/Zombie.hpp
@@ -6,7 +6,7 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 12:34:04 by tkeil             #+#    #+#             */
-/*   Updated: 2025/02/18 12:54:38 by tkeil            ###   ########.fr       */
+/*   Updated: 2025/02/26 13:51:58 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@ public:
     Zombie();
     Zombie(std::string name);
     ~Zombie();
+    void    announce( void ) const;
 };
 
 Zombie* zombieHorde( int N, std::string name );
