bd6077473e925fcdd0e6413705af1011a8b51452
Tobias Keil <tobiaskeil.dev@gmail.com>
2025-04-29 14:02:44 +0200

    ex00: implemented them with templates.

---
+++ cpp07/ex01/Serializer.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Serializer.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 19:26:12 by tkeil             #+#    #+#             */
+/*   Updated: 2025/04/28 21:31:26 by tkeil            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Serializer.hpp"
+
+Serializer::Serializer()
+{
+}
+
+Serializer::Serializer(const Serializer &other)
+{
+    *this = other;
+}
+
+Serializer &Serializer::operator=(const Serializer &other)
+{
+    if (this == &other)
+        return *this;
+    return *this;
+}
+
+Serializer::~Serializer()
+{
+}
+
+uintptr_t Serializer::serialize(Data* ptr)
+{
+    return (reinterpret_cast<uintptr_t>(ptr));
+}
+
+Data* Serializer::deserialize(uintptr_t raw)
+{
+    return (reinterpret_cast<Data*>(raw));
+}
